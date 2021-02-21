21c3c9e03534fef9bd4c242952d23ee7ac9df7ea
Berke <64697416+Gaskmantr@users.noreply.github.com>
2021-02-21 14:26:47 +0300

    Update hackerchat.py

--- hackerchat.py
+++ hackerchat.py
@@ -3,10 +3,21 @@ import websocket
 import threading
 import random
 import requests
-import time
+import time,sys
 import os
 
+os.system('color a')
 os.system('cls')
+print("""
+             __ __     __  __ __   ____  ______ 
+            |  T  T   /  ]|  T  T /    T|      T
+            |  l  |  /  / |  l  |Y  o  ||      |
+            |  _  | /  /  |  _  ||     |l_j  l_j
+            |  |  |/   \_ |  |  ||  _  |  |  |  
+            |  |  |\     ||  |  ||  |  |  |  |  
+            l__j__j \____jl__j__jl__j__j  l__j  
+                                        -Coded by GaskmanTR-
+""")
 
 all_proxies = requests.get('https://api.proxyscrape.com/?request=getproxies&proxytype=http&timeout=1000&country=all&ssl=all&anonymity=all').text
 x = all_proxies.split()
@@ -14,11 +25,18 @@ b = random.choice(x)
 
 deneme = {'http':'http://'+b}
 
-oda = input("Enter room ID:")
+oda = 'lexsecurity'
 
 ws=websocket.create_connection("wss://hack.chat/chat-ws",proxies=deneme)
-print("[+] Connection OK")
+chars = "|/-\|"
+for i in range(30):
+    time.sleep(0.1)
+    sys.stdout.write("\r""["+chars[i % len(chars)]+"]Connecting...")
+    sys.stdout.flush()
 
+time.sleep(0.3)
+print("\n[+] Connection OK")
+time.sleep(0.2)
 nick=input("Enter Nickname: ")
 ws.send(json.dumps({"cmd":"join","channel":"{}".format(oda),"nick":nick}))
 
@@ -42,4 +60,4 @@ while 1:
     data=input("")
     data={"cmd":"chat","text":"\n"+data+"\n"}
     ws.send(json.dumps(data))
-    time.sleep(0.3)
+    time.sleep(0.4)
